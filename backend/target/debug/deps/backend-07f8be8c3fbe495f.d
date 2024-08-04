92e3daab99531d7975b1586cc44fe88e12b6d915
ThiagoOpaula <thiago-oliveira2001@live.com>
2024-08-04 12:51:44 -0300

    first commit

---
+++ backend/target/debug/deps/backend-07f8be8c3fbe495f.d
@@ -0,0 +1,5 @@
+/home/thiago/Documentos/lineSkip/backend/target/debug/deps/libbackend-07f8be8c3fbe495f.rmeta: src/main.rs
+
+/home/thiago/Documentos/lineSkip/backend/target/debug/deps/backend-07f8be8c3fbe495f.d: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/deps/backend-6136978ef12a64f0.d
@@ -0,0 +1,5 @@
+/home/thiago/Documentos/lineSkip/backend/target/debug/deps/libbackend-6136978ef12a64f0.rmeta: src/main.rs
+
+/home/thiago/Documentos/lineSkip/backend/target/debug/deps/backend-6136978ef12a64f0.d: src/main.rs
+
+src/main.rs:
